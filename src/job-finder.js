5b4690b4e432e5abea5c26b170f334e9e6ee9264
Ivo Batistela <irbatistela@gmail.com>
2024-09-04 13:57:34 +0000

    Filter Java jobs

--- src/job-finder.js
+++ src/job-finder.js
@@ -4,18 +4,28 @@ module.exports = (function() {
     const findJobs = async function() {
         const response = await axios.get('anyurl');
 
-        const mappedJobs = response.data.map(function(job) {
-            return {
-                company: 'X-Team',
-                title: job.name,
-                description: job.description
-            };
-        });
-
+        const mappedJobs = response.data
+        .filter(javaJobs)
+        .map(expectedJobStructure);
+        
         return {
             jobs: mappedJobs
         }
     };
+
+    function javaJobs(job) {
+        const matches = job.name.matchAll(/\bjava\b/gi);
+        const hasFoundAnything = !matches.next().done;
+        return hasFoundAnything;
+    }
+
+    function expectedJobStructure(job) {
+        return {
+            company: 'X-Team',
+            title: job.name,
+            description: job.description
+        };
+    }
 
     return {
         findJobs: findJobs
--- tests/job-finder.test.js
+++ tests/job-finder.test.js
@@ -1,73 +1,79 @@
 const axios = require('axios');
 const sinon = require('sinon');
 
-sinon.stub(axios, 'get').resolves({
-  data: [{
-    "id": 607,
-    "name": "Senior Java Developer",
-    "slug": "senior-java-developer-240719",
-    "description": "## Most Important:\n- Strong proficiency in Java programming.\n- Experience with JSP and basic HTML.\n- Strong knowledge of MySQL database management.\n- Proficiency in JavaScript.\n- Excellent debugging and problem-solving skills.\n\n## Nice to Have:\n- Experience with JaCoCo test coverage tool.\n- Familiarity with modern development practices and agile methodologies.\n\n## Personal Attributes:\n- Strong self-organization and execution skills to meet project objectives independently.\n- Strong communication skills in English, both written and verbal.",
-    "summary": null,
-    "created_at": "2024-07-19T11:25:59.000Z",
-    "updated_at": "2024-08-22T16:12:25.000Z",
-    "priority": 8,
-    "open": false,
-    "baseTimezone": "Central Standard Time (North America) CST",
-    "arrangement": null,
-    "jobLocation": null,
-    "image": {},
-    "skills": [
-        {
-            "id": 4,
-            "title": "Java",
-            "slug": "java",
-            "confirmed": true,
-            "created_at": "2020-12-02T02:35:32.000Z",
-            "updated_at": "2020-12-02T02:35:32.000Z"
-        },
-        {
-            "id": 56,
-            "title": "MySQL",
-            "slug": "mysql",
-            "confirmed": true,
-            "created_at": "2020-12-02T02:35:32.000Z",
-            "updated_at": "2020-12-02T02:35:32.000Z"
-        },
-        {
-            "id": 1,
-            "title": "JavaScript",
-            "slug": "javascript",
-            "confirmed": true,
-            "created_at": "2020-12-02T02:35:32.000Z",
-            "updated_at": "2020-12-02T02:35:32.000Z"
-        }
-    ],
-    "isAlwaysHiring": false
-},
-{
-  "id": 607,
-  "name": "Senior Java Developer 2",
-  "slug": "senior-java-developer-240719",
-  "description": "Full description",
-  "summary": null,
-  "created_at": "2024-07-19T11:25:59.000Z",
-  "updated_at": "2024-08-22T16:12:25.000Z",
-  "priority": 8,
-  "open": false,
-  "baseTimezone": "Central Standard Time (North America) CST",
-  "arrangement": null,
-  "jobLocation": null,
-  "image": {},
-  "skills": [
-  ],
-  "isAlwaysHiring": false
-}
-]
-});
+let stub;
 
 const JobFinder = require('../src/job-finder');
 
+afterEach(() => {
+  stub.restore();
+});
+
 test('Return found jobs', async () => {
+  stub = sinon.stub(axios, 'get').resolves({
+    data: [{
+      "id": 607,
+      "name": "Senior Java Developer",
+      "slug": "senior-java-developer-240719",
+      "description": "## Most Important:\n- Strong proficiency in Java programming.\n- Experience with JSP and basic HTML.\n- Strong knowledge of MySQL database management.\n- Proficiency in JavaScript.\n- Excellent debugging and problem-solving skills.\n\n## Nice to Have:\n- Experience with JaCoCo test coverage tool.\n- Familiarity with modern development practices and agile methodologies.\n\n## Personal Attributes:\n- Strong self-organization and execution skills to meet project objectives independently.\n- Strong communication skills in English, both written and verbal.",
+      "summary": null,
+      "created_at": "2024-07-19T11:25:59.000Z",
+      "updated_at": "2024-08-22T16:12:25.000Z",
+      "priority": 8,
+      "open": false,
+      "baseTimezone": "Central Standard Time (North America) CST",
+      "arrangement": null,
+      "jobLocation": null,
+      "image": {},
+      "skills": [
+        {
+          "id": 4,
+          "title": "Java",
+          "slug": "java",
+          "confirmed": true,
+          "created_at": "2020-12-02T02:35:32.000Z",
+          "updated_at": "2020-12-02T02:35:32.000Z"
+        },
+        {
+          "id": 56,
+          "title": "MySQL",
+          "slug": "mysql",
+          "confirmed": true,
+          "created_at": "2020-12-02T02:35:32.000Z",
+          "updated_at": "2020-12-02T02:35:32.000Z"
+        },
+        {
+          "id": 1,
+          "title": "JavaScript",
+          "slug": "javascript",
+          "confirmed": true,
+          "created_at": "2020-12-02T02:35:32.000Z",
+          "updated_at": "2020-12-02T02:35:32.000Z"
+        }
+      ],
+      "isAlwaysHiring": false
+    },
+    {
+      "id": 607,
+      "name": "Senior Java Developer 2",
+      "slug": "senior-java-developer-240719",
+      "description": "Full description",
+      "summary": null,
+      "created_at": "2024-07-19T11:25:59.000Z",
+      "updated_at": "2024-08-22T16:12:25.000Z",
+      "priority": 8,
+      "open": false,
+      "baseTimezone": "Central Standard Time (North America) CST",
+      "arrangement": null,
+      "jobLocation": null,
+      "image": {},
+      "skills": [
+      ],
+      "isAlwaysHiring": false
+    }
+    ]
+  });
+
   const result = await JobFinder.findJobs();
 
   expect(result).toEqual({
@@ -76,10 +82,46 @@ test('Return found jobs', async () => {
         company: 'X-Team',
         title: 'Senior Java Developer',
         description: '## Most Important:\n- Strong proficiency in Java programming.\n- Experience with JSP and basic HTML.\n- Strong knowledge of MySQL database management.\n- Proficiency in JavaScript.\n- Excellent debugging and problem-solving skills.\n\n## Nice to Have:\n- Experience with JaCoCo test coverage tool.\n- Familiarity with modern development practices and agile methodologies.\n\n## Personal Attributes:\n- Strong self-organization and execution skills to meet project objectives independently.\n- Strong communication skills in English, both written and verbal.'
-      },{
+      }, {
         company: 'X-Team',
         title: 'Senior Java Developer 2',
         description: 'Full description'
+      }
+    ]
+  })
+});
+
+test('Filter out only desired jobs', async () => {
+  stub = sinon.stub(axios, 'get').resolves({
+    data: [{
+      "id": 607,
+      "name": "Senior Java Developer",
+      "slug": "senior-java-developer-240719",
+      "description": "## Most Important:\n- Strong proficiency in Java programming.\n- Experience with JSP and basic HTML.\n- Strong knowledge of MySQL database management.\n- Proficiency in JavaScript.\n- Excellent debugging and problem-solving skills.\n\n## Nice to Have:\n- Experience with JaCoCo test coverage tool.\n- Familiarity with modern development practices and agile methodologies.\n\n## Personal Attributes:\n- Strong self-organization and execution skills to meet project objectives independently.\n- Strong communication skills in English, both written and verbal."
+    },
+    {
+      "id": 602,
+      "name": "Senior Fullstack Engineer",
+      "slug": "senior-fullstack-engineer-240717",
+      "description": "Description content does not matter"
+    },
+    {
+      "id": 602,
+      "name": "Senior Javascript Engineer",
+      "slug": "senior-fullstack-engineer-240717",
+      "description": "Description content does not matter"
+    }
+    ]
+  });
+
+  const result = await JobFinder.findJobs();
+
+  expect(result).toEqual({
+    jobs: [
+      {
+        company: 'X-Team',
+        title: 'Senior Java Developer',
+        description: '## Most Important:\n- Strong proficiency in Java programming.\n- Experience with JSP and basic HTML.\n- Strong knowledge of MySQL database management.\n- Proficiency in JavaScript.\n- Excellent debugging and problem-solving skills.\n\n## Nice to Have:\n- Experience with JaCoCo test coverage tool.\n- Familiarity with modern development practices and agile methodologies.\n\n## Personal Attributes:\n- Strong self-organization and execution skills to meet project objectives independently.\n- Strong communication skills in English, both written and verbal.'
       }
     ]
   })
